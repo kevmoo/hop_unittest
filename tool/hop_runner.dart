0742b7cd15ed46b07db94027076c701c4efebbfb
Kevin Moore <github@j832.com>
2014-08-27 18:31:53 -0700

    v0.1.0+2

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -1,15 +1,12 @@
 library hop_runner;
 
 import 'package:hop/hop.dart';
-import 'package:hop_docgen/hop_docgen.dart';
 import 'package:hop_unittest/hop_unittest.dart';
 
 import '../test/harness_console.dart' as test;
 
 void main(List<String> args) {
   addTask('test', createUnitTestTask(test.main));
-
-  addTask('docs',  createDocGenTask('../compiled_dartdoc_viewer'));
 
   runHop(args);
 }
